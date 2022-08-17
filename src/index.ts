1b4fff9d238e6f0daad3a9fbd47e6fadad0409c6
Joe Grinstead <npm@mailforce.net>
2022-08-17 15:34:52 -0400

    Start adding support code.
    I stripped some of this from wattle (an engine I have been working on).
    
    Because I do not want to bother with a packager yet, I have just decided to have all the js in one monolithic file.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,164 @@
+/****************************************************************************
+ *
+ * @file raii
+ *
+ * RAII is a useful concept from C++.
+ *
+ * The important idea is that things get cleaned up when they go out-of-scope.
+ *
+ ****************************************************************************/
 
-const world = 'world';
+/**
+ * Work that we need to run when we are finished using the active resource.
+ *
+ * This is a stack, so the code will be called in reverse-order, though that
+ * should not really be used.
+ */
+type CleanupStack = (() => void)[];
 
-export function hello(who: string = world): string {
-	return `Hello ${who}`;
+/**
+ * Associates objects with the code we need to run on "cleanup" of those
+ * objects. We use a standard Map here instead of a WeakMap because these
+ * objects must be cleaned.
+ */
+const ACTIVE_RESOURCES: Map<unknown, CleanupStack> = new Map();
+
+/**
+ * This is defined if we are in a `runAndCleanOnError` call. It is an array of
+ * code to call when cleaning up
+ */
+let activeCleanupStack: null | CleanupStack = null;
+
+/**
+ * Runs code that we are worried might throw an exception. If it does, then
+ * the registered clean-up functions will be called (they are registered by
+ * calling the `ifError` function)
+ */
+function runAndCleanOnError<T>(code: () => T): T {
+	return _runAndCleanOnError(code)[0];
 }
+
+function allocate<T>(code: () => T): T {
+	const [resource, cleanupStack] = _runAndCleanOnError(code);
+
+	// will there ever be a prior, seems like a mistake?
+	const prior = ACTIVE_RESOURCES.get(resource);
+
+	if (prior) {
+		prior.push(...cleanupStack);
+	} else {
+		ACTIVE_RESOURCES.set(resource, cleanupStack);
+
+		// automatically cleanup if the parent is cleaning
+		activeCleanupStack?.push(() => {
+			release(resource);
+		});
+	}
+
+	return resource;
+}
+
+function release(resource: Object) {
+	const cleanupStack = ACTIVE_RESOURCES.get(resource);
+
+	if (cleanupStack) {
+		ACTIVE_RESOURCES.delete(resource);
+		runCleanup(cleanupStack);
+	} else {
+		console.warn(`unrecognized release`, resource);
+	}
+}
+
+function onRelease(cleanup: () => void) {
+	if (activeCleanupStack) {
+		activeCleanupStack.push(cleanup);
+	} else {
+		throw new Error(`onRelease called outside of allocation`);
+	}
+}
+
+function _runAndCleanOnError<T>(code: () => T): [T, CleanupStack] {
+	const parentStack = activeCleanupStack;
+	const cleanupStack: CleanupStack = [];
+	activeCleanupStack = cleanupStack;
+
+	let success = false;
+	try {
+		const result = code();
+		success = true;
+		return [result, cleanupStack];
+	} finally {
+		// using a finally block to avoid re-throwing the error (which I think,
+		// but have not verified, will mess with the stack)
+
+		activeCleanupStack = parentStack;
+
+		if (!success) runCleanup(cleanupStack);
+	}
+}
+
+/**
+ * Calls everything in the stack, will output any thrown exceptions to the
+ * console.
+ */
+function runCleanup(cleanupCode: CleanupStack) {
+	let i = cleanupCode.length;
+	while (i--) {
+		const clean = cleanupCode[i];
+		try {
+			clean();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+}
+
+/****************************************************************************
+ *
+ * @file shader
+ *
+ ****************************************************************************/
+
+function buildShader(gl: WebGLRenderingContext, vertexShader: boolean) {
+	const shaderEnum = vertexShader ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER;
+
+	const shader = gl.createShader(shaderEnum);
+}
+
+/****************************************************************************
+ *
+ * @file exe
+ *
+ ****************************************************************************/
+
+function onWindowEvent(event: string, handler: () => void) {
+	window.addEventListener(event, handler);
+}
+
+function run() {
+	const canvas = document.createElement("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	const body = document.body;
+
+	const gl = canvas?.getContext("webgl");
+	if (!gl) throw "Please update browser";
+
+	body.innerHTML = "";
+	body.appendChild(canvas);
+
+	console.log(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+	gl.clearColor(0, 0.5, 0, 1);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+onWindowEvent("load", () => {
+	try {
+		runAndCleanOnError(run);
+	} catch (error) {
+		document.body.innerText = String(error);
+	}
+});
