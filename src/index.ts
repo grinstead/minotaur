863f4f6b256f06a7f09b34f9837e8f7fea4f55d4
Joe Grinstead <npm@mailforce.net>
2022-08-31 12:08:56 -0400

    Start beginnings of projection

--- src/index.ts
+++ src/index.ts
@@ -398,6 +398,39 @@ function makeWallBlocks() {
 
 /****************************************************************************
  *
+ * @file matrices
+ *
+ ****************************************************************************/
+
+// always 4x4
+type Matrix = Float32Array;
+
+function mult(A: Matrix, B: Matrix): Matrix {
+	// Multiply A and B... beautiful...
+	const matrix = new Float32Array([
+		A[0] * B[0] + A[1] * B[4] + A[2] * B[8] + A[3] * B[12],
+		A[0] * B[1] + A[1] * B[5] + A[2] * B[9] + A[3] * B[13],
+		A[0] * B[2] + A[1] * B[6] + A[2] * B[10] + A[3] * B[14],
+		A[0] * B[3] + A[1] * B[7] + A[2] * B[11] + A[3] * B[15],
+		A[4] * B[0] + A[5] * B[4] + A[6] * B[8] + A[7] * B[12],
+		A[4] * B[1] + A[5] * B[5] + A[6] * B[9] + A[7] * B[13],
+		A[4] * B[2] + A[5] * B[6] + A[6] * B[10] + A[7] * B[14],
+		A[4] * B[3] + A[5] * B[7] + A[6] * B[11] + A[7] * B[15],
+		A[8] * B[0] + A[9] * B[4] + A[10] * B[8] + A[11] * B[12],
+		A[8] * B[1] + A[9] * B[5] + A[10] * B[9] + A[11] * B[13],
+		A[8] * B[2] + A[9] * B[6] + A[10] * B[10] + A[11] * B[14],
+		A[8] * B[3] + A[9] * B[7] + A[10] * B[11] + A[11] * B[15],
+		A[12] * B[0] + A[13] * B[4] + A[14] * B[8] + A[15] * B[12],
+		A[12] * B[1] + A[13] * B[5] + A[14] * B[9] + A[15] * B[13],
+		A[12] * B[2] + A[13] * B[6] + A[14] * B[10] + A[15] * B[14],
+		A[12] * B[3] + A[13] * B[7] + A[14] * B[11] + A[15] * B[15],
+	]);
+
+	return matrix;
+}
+
+/****************************************************************************
+ *
  * @file maze generation
  *
  ****************************************************************************/
@@ -513,18 +546,14 @@ type RenderInfo = {
 
 type GameInfo = {
 	maze: Maze;
-	render: RenderInfo;
+	renderInfo: RenderInfo;
 };
 
 function render(info: GameInfo) {
 	const {
 		maze,
-		render: { gl, dim, wallProgram, wallBlocks, attribute, uniform },
+		renderInfo: { gl, dim, wallProgram, wallBlocks, attribute, uniform },
 	} = info;
-
-	const pxPerBlock = Math.floor(
-		(0.875 * Math.min(dim.width, dim.height)) / MAZE_SIDE
-	);
 
 	gl.useProgram(wallProgram);
 	gl.enable(gl.DEPTH_TEST);
@@ -536,8 +565,11 @@ function render(info: GameInfo) {
 	gl.enableVertexAttribArray(attribute.normal);
 	gl.vertexAttribPointer(attribute.normal, 3, gl.FLOAT, false, 24, 12);
 
-	const cameraX = 0;
-	const cameraY = 0;
+	/*
+	const pxPerBlock = Math.floor(
+		(0.875 * Math.min(dim.width, dim.height)) / MAZE_SIDE
+	);
+
 	const xScale = (2 * pxPerBlock) / dim.width;
 	const yScale = (2 * pxPerBlock) / dim.height;
 
@@ -547,6 +579,15 @@ function render(info: GameInfo) {
 		0, yScale, 1 / 32, +0.0,
 		0, yScale / 2, +0.0, +0.0,
 		-(camera.x * xScale), -(camera.y * yScale), +0.0, +1.0,
+	]);
+	*/
+
+	// prettier-ignore
+	const cameraMatrix = new Float32Array([
+		1/2, 0, +0.0, +0.0,
+		0, 0, 1 / MAZE_SIDE, 1,
+		0, 1, +0.0, +0.0,
+		camera.x, -.6, -camera.y / MAZE_SIDE, 0,
 	]);
 
 	gl.uniformMatrix4fv(uniform.projection, false, cameraMatrix);
@@ -673,7 +714,7 @@ void main() {
 		},
 	};
 
-	render({ maze, render: renderInfo });
+	render({ maze, renderInfo });
 }
 
 function onKeyboard() {}
